279293c306f41849e11dabb03446386edc422234
kaustdixit <kaustdixit@deloitte.com>
2022-12-05 23:22:02 +0530

    API server (products) created and ready for data-entry

---
+++ miniecom-server/models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    imageurl: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('product', productSchema);
