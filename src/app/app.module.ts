83d0e3e9fa79f6b15d0320f152dd4a6a5e882c50
kaustdixit <kaustdixit@deloitte.com>
2022-12-06 22:44:07 +0530

    added basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ListingpageComponent } from './listingpage/listingpage.component';
+import { ProductpageComponent } from './productpage/productpage.component';
+import { CarouselComponent } from './homepage/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
+    ListingpageComponent,
+    ProductpageComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
